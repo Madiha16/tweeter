d96fd0de3e70b7af9d86f7d3edd58fc170870add
Your Name <your@email.com>
2022-03-04 06:33:22 -0500

    create HTML markup using template literals in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  // const createTweetElement = function() {
+  //   // const $article = $(`<article class="tweet"></article>`);
+  //   // $article.children()
+
+  // };
+
+  const createTweetElement = `
+  <article class="user">
+    <header>
+      <figure>
+        <img src=${tweetData.user.avatars}>
+        <figcaption>
+          ${tweetData.user.name}
+        </figcaption>
+      </figure>
+        <small>${tweetData.user.handle}</small>
+    </header>
+    <strong>${tweetData.content.text}</strong>
+    <footer>
+      <sub>
+        <span>
+          <date>${tweetData.created_at}</date>
+        </span>
+      </sub>
+    </footer>
+  `;
+  
+  console.log("createTweetElement::", createTweetElement);
+
+  // const $tweet = $(`<article class="tweet">Hello world</article>`);
+  // console.log("$tweet:", $tweet);// $tweet: k.fn.init(1)
+
+  // const $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,25 +5,32 @@ $(() => {
     // create max char variable
     const maxlength = 140;
 
-    // Use this to grab the value of the textarea in question
+    // Use 'this' to grab the value of the textarea in question
     // textarea tag has id of: #tweet-text
     const tweetText = $(this).val();
-    // console.log("tweetText:", tweetText);
+    // console.log("tweetText:", tweetText);// gives back text in textarea
 
     // and determine the length of that input value
     const tweetLength = $(this).val().length;
-    // console.log("tweetLength:", tweetLength);
+    // console.log("tweetLength:", tweetLength);// counts characters in textarea
 
     // const counter = $('.counter').val();// 140
     const children = $(this).parent().children()[2];
     const $children = $(children).children()[1];
     const counter = $($children);// the counter is a DOM element, using jQuery to manipulate it
     // let counterVal = $($children).text(); // 140
-    console.log("counter:", counter);
-    let innerText = counter[0].innerText;// 140
-    counter[0].innerText = maxlength - tweetLength;
+    // console.log("counter:", counter);
+
+    // set innerText to counter[0] innnerText value (140)
+    // let innerText = counter[0].innerText;// 140
     // console.log("innerText:", innerText);
 
+    // set counter's innerText equal to -> maxLength - the tweetLength value (increases or decreases depending on input)
+    counter[0].innerText = maxlength - tweetLength;
+
+    // if the counter' inner text is less than 0, call a jQuery function on it
+    //which takes in a string, which is styled red in new-tweet.css sheet
+    // OTHERWISE, remove the class (if not less than 0, when characters over limit are deleted)
     // 'negative' is a string which matches the css class we named it
     if (counter[0].innerText < 0) {
       counter.addClass('negative');
