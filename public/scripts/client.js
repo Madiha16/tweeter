7c13879a5ec17d18e9738b6a196426aa5dc0db6b
Your Name <your@email.com>
2022-03-04 21:59:46 -0500

    Remove  hard-coded tweet object from client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 
 $(() => {
 
-  // // Fake data taken from initial-tweets.json
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
-
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -90,25 +64,6 @@ $(() => {
     });
 
   });
-
-  // // Rabbit example
-  // const fetchRabbits = () => {
-  //   $.ajax({
-  //     url: '/rabbits',
-  //     method: 'GET'
-  //   }).then((rabbits) => {
-  //     console.log(rabbits);
-
-  //     // empty the parent element before we append new children to it
-  //     $wereRabbitContainer.empty();
-
-  //     for (const rabbitKey in rabbits) {
-  //       console.log('rabbitKey', rabbitKey);
-  //       const $wereRabbit = createWereRabbit(rabbits[rabbitKey]);
-  //       $wereRabbitContainer.append($wereRabbit);
-  //     }
-  //   });
-  // };
   
   // The loadtweets function will use jQuery to make an AJAX GET request to /tweets
   // and receive the array of tweets as JSON.
@@ -119,7 +74,7 @@ $(() => {
     }).then((tweets) => {
       console.log(tweets);
 
-      // // empty the parent element before we append new children to it
+      // // ??? empty the parent element before we append new children to it
       // $('#tweets-container').empty();
 
       renderTweets(tweets);
@@ -129,7 +84,5 @@ $(() => {
   // In order to test/drive the function, you can simply call it right after its definition.
   // We do want to load the tweets on page load anyway, so this is fair.
   loadTweets();
-
-  // Remove the hard-coded tweets object that we added earlier in order to drive our renderTweets function.
   
 });
